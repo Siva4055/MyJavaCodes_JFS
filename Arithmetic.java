14771aaba46e4804a0a3e01842f1c39a631767a3
Siva4055 <sivadastagiri425@gmail.com>
2025-04-25 23:52:22 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,23 @@
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int num1=5;
+         int num2=10;
+         int add=num1 + num2;
+         int sub=num1-num2;
+         int div=num1 /num2;
+         int mul=num1 *num2;
+         int mod=num1%num2;
+         System.out.println(add);
+         System.out.println(sub);
+         System.out.println(div);
+         System.out.println(mul);
+         System.out.println(mod);
+         
+        
+         
+
+	}
+}
